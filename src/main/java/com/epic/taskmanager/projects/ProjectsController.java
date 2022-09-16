917ea3548926afc6af70bac87d94c3d61fb3e93e
d-kondratenko <dmitkondratenko@gmail.com>
2022-09-16 10:59:26 +0300

    add project controller

---
+++ src/main/java/com/epic/taskmanager/projects/ProjectsController.java
@@ -0,0 +1,11 @@
+package com.epic.taskmanager.projects;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectsController {
+
+
+}
