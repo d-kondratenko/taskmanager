1b1c4dba7e205efbd16c4949f31ff3335452d671
d-kondratenko <dmitkondratenko@gmail.com>
2022-09-16 10:58:14 +0300

    add project dto

---
+++ src/main/java/com/epic/taskmanager/projects/dto/ProjectDto.java
@@ -0,0 +1,12 @@
+package com.epic.taskmanager.projects.dto;
+
+import lombok.Data;
+
+@Data
+public class ProjectDto {
+    String projectName;
+    String projectDescription;
+    String userNr;
+    String token;
+
+}
---
+++ src/main/java/com/epic/taskmanager/projects/dto/ProjectTypesDto.java
@@ -0,0 +1,10 @@
+package com.epic.taskmanager.projects.dto;
+
+import lombok.Data;
+
+@Data
+public class ProjectTypesDto {
+    String projectTypeNr;
+    String userNr;
+    String token;
+}
